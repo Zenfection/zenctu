3b968d3ff6a7feede9d665dc98f4bd02ca12ed52
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2021-08-30 14:30:00 +0700

    update

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,72 @@
+import { defineUserConfig } from '@vuepress/cli'
+import type { DefaultThemeOptions } from '@vuepress/theme-default'
+import { path } from '@vuepress/utils'
+import { navbar, sidebar } from './configs'
+
+
+const isProd = process.env.NODE_ENV === 'production'
+
+export default defineUserConfig<DefaultThemeOptions>({
+  base: '/',
+  head: [
+    ['link',{rel: 'icon',type: 'image/png',sizes: '16x16',href: `/images/icons/favicon-16x16.png`,},],
+    ['link',{rel: 'icon',type: 'image/png',sizes: '32x32',href: `/images/icons/favicon-32x32.png`,},],
+    ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
+    ['meta', { name: 'application-name', content: 'ZenCTU' }],
+    ['meta', { name: 'apple-mobile-web-app-title', content: 'ZenCTU' }],
+    ['meta',{ name: 'apple-mobile-web-app-status-bar-style', content: 'black' },],
+    ['link',{ rel: 'apple-touch-icon', href: `/images/icons/apple-touch-icon.png` },],
+    ['link',{rel: 'mask-icon',href: '/images/icons/safari-pinned-tab.svg'},],
+    ['meta', { name: 'msapplication-TileColor', content: '#06bdf8' }],
+    ['meta', { name: 'theme-color', content: '#06bdf8' }],
+  ],
+
+  // site-level locales config
+  locales: {
+    '/': {
+      lang: 'vi',
+      title: 'Zen Course Guide',
+      description: 'Môi trường học tập lý tưởng cho sinh viên Công Nghệ Thông Tin'
+    },
+  },
+
+  bundler:
+    // specify bundler via environment variable
+    process.env.DOCS_BUNDLER ??
+    // use vite in dev, use webpack in prod
+    (isProd ? '@vuepress/webpack' : '@vuepress/vite'),
+
+  themeConfig: {
+    logo: '/images/hero.png',
+    lang: 'vi',
+    darkMode: true,
+    docsRepo: 'zenfection/zenctu',
+    editLink: false,
+    docsDir: 'docs',
+    theme: 'dark',
+    // theme-level locales config
+    locales: {
+      '/': {
+        navbar: navbar.vi,
+        sidebar: sidebar.vi,
+
+        // page meta
+        lastUpdatedText:"Cập nhật lúc ",
+        contributorsText:'Tác giả'
+      },
+    },
+    themePlugins: {git: isProd,},
+  },
+
+  markdown: {importCode: {handleImportPath: (str) =>str.replace(/^@vuepress/,path.resolve(__dirname, '../../packages/@vuepress')),},},
+
+  plugins: [
+    // ['@vuepress/plugin-debug'],
+    ['@vuepress/plugin-docsearch',{appId: '3CJDV2AFXL',apiKey: '4f7f93d347463109c3b6fd21d3ac2424',indexName: 'ctuvn',placeholder:"Tìm Kiếm..."},],
+    ['@vuepress/plugin-google-analytics',{ id: 'G-1GE3D8ZGM4', },],
+    ['@vuepress/plugin-pwa'],
+    ['@vuepress/plugin-pwa-popup',{locales: {'/': {message: 'Phiên bản mới đã lên',buttonText: 'Update!!!',},}}],
+    ['@vuepress/plugin-register-components',{componentsDir: path.resolve(__dirname, './components'),},],
+  ],
+  clientAppRootComponentFiles: path.resolve(__dirname, './components/Cat.vue'),
+})
---
+++ docs/.vuepress/configs/navbar/vi.ts
@@ -0,0 +1,112 @@
+import type { NavbarConfig } from '@vuepress/theme-default'
+
+//! Các môn chưa hỗ trợ
+  //'/nhapmon/CT190-Nhap_mon_tri_tue_nhan_tao/',
+  //'/nhapmon/CT173-Kien_truc_may_tinh/',
+  //'/cosonganh/CT178-Nguyen_ly_he_dieu_hanh/',
+  //'/chuyennganh/CT273-Giao_dien_nguoi_may/',
+  //'/chuyennganh/CT293-Mang_va_truyen_thong_du_lieu/',
+  //'/doannganh/CT271-Nien_luan_co_so_CNTT/',
+  //'/doannganh/CT446-Nien_luan_CNTT/',
+export const vi: NavbarConfig = [
+  {
+    text: 'Trang Chủ',
+    link: '/guide/intro.md'
+  },
+  {
+    text: 'Đại Cương',
+    children: [
+      {
+        text: 'Toán Học',
+        children: [
+          '/toanhoc/TN001-Vi_tich_phan_a1/',
+          '/toanhoc/TN002-Vi_tich_phan_a2/',
+          '/toanhoc/TN010-Xac_suat_thong_ke/',
+          '/toanhoc/TN012-Dai_so_tuyen_tinh_va_hinh_hoc/',
+        ]
+      },
+      {
+        text: 'Tin Học',
+        children: [
+          '/nhapmon/CT200-Nen_tang_cong_nghe_thong_tin/',
+        ]
+      }
+    ]
+  },
+  {
+    text: 'Nền Tảng',
+    children: [
+      {
+        text: 'Nhập Môn',
+        children: [
+          '/nhapmon/CT101-Lap_trinh_can_ban_a/',
+          '/nhapmon/CT172-Toan_roi_rac/',
+          '/nhapmon/CT179-Quan_tri_he_thong/',
+          '/nhapmon/CT188-Nhap_mon_lap_trinh_web/',
+        ],
+      },
+      {
+        text: 'Cơ Sở',
+        children: [
+          '/cosonganh/CT112-Mang_may_tinh/',
+          '/cosonganh/CT174-Phan_tich_va_thiet_ke_thuat_toan/',
+          '/cosonganh/CT175-Ly_thuyet_do_thi/',
+          '/cosonganh/CT176-Lap_trinh_huong_doi_tuong/',
+          '/cosonganh/CT177-Cau_truc_du_lieu/',
+          '/cosonganh/CT180-Co_so_du_lieu/',
+          '/cosonganh/CT182-Ngon_ngu_mo_hinh_hoa/',
+          '/cosonganh/CT296-Phan_tich_thiet_ke_he_thong/',
+        ],
+      },
+    ],
+  },
+  {
+    text: 'Nâng cao',
+    children: [
+      {
+        text: 'Chuyên Ngành',
+        children: [
+          '/chuyennganh/CT222-An_toan_he_thong/',
+          '/chuyennganh/CT223-Quan_ly_du_an_phan_mem/',
+          '/chuyennganh/CT275-Cong_nghe_web/',
+          '/chuyennganh/CT294-May_hoc_ung_dung/',
+          '/chuyennganh/CT467-Quan_tri_du_lieu/',
+        ]
+      },
+      {
+        text: 'Đồ Án Ngành',
+        children: [
+          '/doannganh/CT300-Phat_trien_phan_mem/',
+          '/doannganh/CT449-Phat_trien_ung_dung_web/',
+          '/doannganh/CT484-Phat_trien_ung_dung_tren_thiet_bi_di_dong/',
+          '/doannganh/CT550-Luan_van_tot_nghiep/',
+        ],
+      }
+    ],
+  },
+
+  {
+    text: 'Tài Nguyên',
+    children: [
+
+      {
+        text: 'Khóa Học',
+        children:[
+          { text: 'Front-end', link: '/course/frontend/'},
+          { text: 'Back-end', link: '/course/backend/'},
+          { text: 'Server', link: '/course/server/'},
+          { text: 'System', link: '/course/system/'},
+          { text: 'Mobile', link: '/course/mobile/'},
+          { text: 'Office', link: '/course/office/'},
+        ]
+      },
+      {
+        text: 'Khác',
+        children:[
+          {text: 'Tips', link: '/tips/'},
+          {text: 'DevBook', link: '/devbook/'},
+        ]
+      },
+    ]
+  },
+]
