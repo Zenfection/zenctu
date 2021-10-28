031a317af0b1227d2f64004b1d6bf3b3c8b8c105
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2021-10-28 21:14:53 +0700

    update

--- packages/@vuepress/bundler-vite/src/dev/createDev.ts
+++ packages/@vuepress/bundler-vite/src/dev/createDev.ts
@@ -1,4 +1,5 @@
 import type { App, Bundler } from '@vuepress/core'
+import { chalk } from '@vuepress/utils'
 import { createServer, mergeConfig } from 'vite'
 import { createPlugin } from '../plugin'
 import type { ViteBundlerOptions } from '../types'
@@ -26,6 +27,16 @@ export const createDev = (
   const server = await createServer(viteConfig)
 
   await server.listen()
+
+  server.config.logger.info(
+    chalk.cyan(`\n  vite v${require('vite/package.json').version}`) +
+      chalk.green(` dev server running at:\n`),
+    {
+      clear: !server.config.logger.hasWarned,
+    }
+  )
+
+  server.printUrls()
 
   return server.close.bind(server)
 }
